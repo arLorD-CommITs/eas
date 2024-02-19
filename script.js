b1e1aa5cec83520a247813f28d201e3c746b5098
Christian Arlu Laderas <christianarlu@Xubuntu2024.myguest.virtualbox.org>
2024-02-19 09:31:13 +0800

    Add a new button and function
    The new button provides the ability to overwrite the grid. The event listener 'click'
    is used to activate the button. I also created a new function that removes all squares.

--- script.js
+++ script.js
@@ -1,21 +1,48 @@
 const mainContainer = document.createElement('div');
 const htmlBody = document.querySelector('body');
+const button = document.createElement('button');
+const buttonText = document.createTextNode('Click me!');
 
+button.appendChild(buttonText);
 htmlBody.appendChild(mainContainer);
+htmlBody.appendChild(button);
 mainContainer.setAttribute('class', 'mainContainer');
 
-for (let i = 0; i < 256; i++) {
-    let box = document.createElement('div');
-    mainContainer.appendChild(box);
+let nOfBoxes = 256;
+
+function createGrid(nOfBoxes) {
+    for (let i = 0; i < nOfBoxes; i++) {
+        let box = document.createElement('div');
+        mainContainer.appendChild(box);
+    }
 }
 
-const boxes = document.querySelectorAll('mainContainer div');
+createGrid(nOfBoxes);
+
+
+const boxes = document.querySelectorAll('.mainContainer div');
 boxes.forEach(function(item) {
-    item.addEventListener('mouseover', mouseOver());
+    item.addEventListener('mouseover', function () {
+        let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        this.style.backgroundColor = randomColor;
+    });
 });
 
-function mouseOver(){
-    alert('hovered');
-    //let colorRandomizer = '#' + Math.floor(Math.random()*16777215).toString(16); 
-    //this.style.backgroundColor = colorRandomizer;
+button.addEventListener('click', function() {
+    let popUp = prompt('How many squares per side would you like the new grid to have?');
+    Math.floor(+popUp);
+
+    if (popUp > 100) {
+        alert('Please enter a value less than 100');
+    } else if (popUp <= 100 && popUp >= 1) {
+        removeBoxes();
+    } else {
+        alert('Invalid input');
+    }
+});
+
+function removeBoxes() {
+    for ( let i = boxes.length - 1; i >= 0; i-- ) {
+        boxes[i].remove()
+    }
 }
